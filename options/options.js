6643a8f99fcf6c32c977a3c3105ffd8e7cf49e56
Crowdbotics <team@crowdbotics.com>
2024-03-19 19:19:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spongebob-themed-mm-47142",
+  url: "https://spongebob-themed-mm-47142.botics.co",
+  api: "https://spongebob-themed-mm-47142.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
